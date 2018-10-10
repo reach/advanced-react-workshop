8aa52ac6201451802d783a35f58e7c5ad6f64cd0
Ryan Florence <rpflorence@gmail.com>
2018-10-10 10:57:03 -0400

    fix import

--- 02-hocs-render-props/lecture/src/index.js
+++ 02-hocs-render-props/lecture/src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./lib/index.css";
 
-import App from "./App.start.hoc";
+import App from "./App.start";
 
 ReactDOM.render(<App />, document.getElementById("root"));
