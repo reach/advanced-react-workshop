deacc2b133c705b76e020f5523deaf7c0a923c69
Ryan Florence <rpflorence@gmail.com>
2018-10-10 14:10:53 -0400

    yeeeeeargghhh

--- 03-clone-element/lecture/src/index.js
+++ 03-clone-element/lecture/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./lib/index.css";
-import App from "./App.context";
+import App from "./App.start";
 
 ReactDOM.render(<App />, document.getElementById("root"));
