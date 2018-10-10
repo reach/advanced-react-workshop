1bfa55c4fea660b0fe14aaf6bd7698cc14f298f2
Ryan Florence <rpflorence@gmail.com>
2018-10-09 22:43:45 -0400

    Update install script

--- scripts/install.js
+++ scripts/install.js
@@ -3,30 +3,16 @@ const shell = require("shelljs");
 [
   "01-imperative-to-declarative",
   "02-hocs-render-props",
-  "03-context-clone-element",
-  "04-portals",
-  "05-wai-aria",
-  "06-gsbu",
-  "07-gdsfp",
-  "08-suspense"
+  "03-clone-element",
+  "04-context",
+  "05-portals",
+  "06-wai-aria",
+  "07-gsbu",
+  "08-gdsfp",
+  "09-suspense"
 ].forEach(dir => {
   shell.cd(`${dir}/exercise`);
   shell.echo(`\nInstalling exercise ${dir}:`);
-  shell.exec(`yarn`);
-  shell.cd("../..");
-});
-
-[
-  "01-imperative-to-declarative",
-  "02-hocs-render-props",
-  "03-context-clone-element",
-  "04-portals",
-  "06-gsbu",
-  "07-gdsfp",
-  "08-suspense"
-].forEach(dir => {
-  shell.cd(`${dir}/lecture`);
-  shell.echo(`\nInstalling lecture ${dir}:`);
   shell.exec(`yarn`);
   shell.cd("../..");
 });
