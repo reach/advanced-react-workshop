6b828b938e27d7c18b40be6f9fd67223fa836199
Ryan Florence <rpflorence@gmail.com>
2019-02-07 10:36:27 -0800

    use more hooks

--- 04-context/lecture/src/App.final.js
+++ 04-context/lecture/src/App.final.js
@@ -1,139 +1,86 @@
-import React, {
-  Component,
-  createContext
-} from "react";
+import React, { createContext, useState, useMemo, useContext } from "react";
 import FaAutomobile from "react-icons/lib/fa/automobile";
 import FaBed from "react-icons/lib/fa/bed";
 import FaPlane from "react-icons/lib/fa/plane";
 import FaSpaceShuttle from "react-icons/lib/fa/space-shuttle";
 import * as text from "./lib/text";
 
-let TabsContext = createContext();
+const TabsContext = createContext();
 
-class Tabs extends Component {
-  state = {
-    activeIndex: 0,
-    selectTabIndex: activeIndex => {
-      this.setState({ activeIndex });
-    }
-  };
+function Tabs({ children }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const context = useMemo(() => {
+    return {
+      activeIndex,
+      onSelectTab: setActiveIndex
+    };
+  }, [activeIndex]);
 
-  render() {
-    return (
-      <TabsContext.Provider value={this.state}>
-        <div className="Tabs">
-          {this.props.children}
-        </div>
-      </TabsContext.Provider>
-    );
-  }
+  return (
+    <TabsContext.Provider value={context}>
+      <div className="Tabs">{children}</div>
+    </TabsContext.Provider>
+  );
 }
 
-let TabList = props => {
-  return (
-    <TabsContext.Consumer>
-      {context => {
-        let children = React.Children.map(
-          props.children,
-          (child, index) => {
-            return React.cloneElement(child, {
-              isActive: index === context.activeIndex,
-              onActivate: () =>
-                context.selectTabIndex(index)
-            });
-          }
-        );
-        return <div className="tabs">{children}</div>;
-      }}
-    </TabsContext.Consumer>
-  );
-};
+function TabList({ children }) {
+  const { activeIndex, onSelectTab } = useContext(TabsContext);
+  const clones = React.Children.map(children, (child, index) => {
+    return React.cloneElement(child, {
+      isActive: index === activeIndex,
+      onSelect: () => onSelectTab(index)
+    });
+  });
+  return <div className="tabs">{clones}</div>;
+}
 
-let Tab = props => {
-  const isDisabled = props.isDisabled;
-  const isActive = props.isActive;
+function Tab({ isActive, isDisabled, onSelect, children }) {
   return (
     <div
-      className={
-        isDisabled
-          ? "tab disabled"
-          : isActive ? "tab active" : "tab"
-      }
-      onClick={
-        isDisabled ? undefined : props.onActivate
-      }
+      className={isDisabled ? "tab disabled" : isActive ? "tab active" : "tab"}
+      onClick={isDisabled ? null : onSelect}
     >
-      {props.children}
+      {children}
     </div>
   );
-};
+}
 
-let TabPanels = props => {
+function TabPanels({ children }) {
+  const { activeIndex } = useContext(TabsContext);
+  return <div className="panels">{children[activeIndex]}</div>;
+}
+
+function TabPanel({ children }) {
+  return children;
+}
+
+function App() {
   return (
-    <TabsContext.Consumer>
-      {context => (
-        <div className="panels">
-          {props.children[context.activeIndex]}
-        </div>
-      )}
-    </TabsContext.Consumer>
+    <div className="App">
+      <Tabs>
+        <TabList>
+          <Tab>
+            <FaAutomobile />
+          </Tab>
+          <Tab>
+            <FaBed />
+          </Tab>
+          <Tab>
+            <FaPlane />
+          </Tab>
+          <Tab>
+            <FaSpaceShuttle />
+          </Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>{text.cars}</TabPanel>
+          <TabPanel>{text.hotels}</TabPanel>
+          <TabPanel>{text.flights}</TabPanel>
+          <TabPanel>{text.space}</TabPanel>
+        </TabPanels>
+      </Tabs>
+    </div>
   );
-};
-
-let TabPanel = props => props.children;
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Tabs>
-          <TabList>
-            <Tab>
-              <FaAutomobile />
-            </Tab>
-            <Tab>
-              <FaBed />
-            </Tab>
-            <Tab>
-              <FaPlane />
-            </Tab>
-            <Tab>
-              <FaSpaceShuttle />
-            </Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>{text.cars}</TabPanel>
-            <TabPanel>
-              <Tabs>
-                <TabList>
-                  <Tab>
-                    <FaAutomobile />
-                  </Tab>
-                  <Tab isDisabled>
-                    <FaBed />
-                  </Tab>
-                  <Tab>
-                    <FaPlane />
-                  </Tab>
-                  <Tab>
-                    <FaSpaceShuttle />
-                  </Tab>
-                </TabList>
-                <TabPanels>
-                  <TabPanel>{text.cars}</TabPanel>
-                  <TabPanel>{text.hotels}</TabPanel>
-                  <TabPanel>{text.flights}</TabPanel>
-                  <TabPanel>{text.space}</TabPanel>
-                </TabPanels>
-              </Tabs>
-            </TabPanel>
-            <TabPanel>{text.flights}</TabPanel>
-            <TabPanel>{text.space}</TabPanel>
-          </TabPanels>
-        </Tabs>
-      </div>
-    );
-  }
 }
 
 export default App;
